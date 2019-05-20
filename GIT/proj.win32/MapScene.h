d28f58372c97858a44c14626c2605b4c0c4bde02
only976 <42378887+only976@users.noreply.github.com>
2019-05-20 14:25:34 +0800

    实现人物动画

--- GIT/proj.win32/MapScene.h
+++ GIT/proj.win32/MapScene.h
@@ -2,14 +2,15 @@
 #define _MAP_SCENE_H_
 
 #include "cocos2d.h"
-
+#include "heroPrint.h"
 class MapScene : public cocos2d::Layer
 {
 public:
 	static cocos2d::Scene* createScene();
 
 	virtual bool init();
-
+	heroPrint* hero;
+	virtual bool onTouchBegan(Touch *touch, Event *unused_event);
 	void EnterHelloWorldScene(Ref *pSenderBack);
 
 
