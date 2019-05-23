065978e8e6d51a4a5556af270a654365f57709e9
EternalHikari <45036969+EternalHikari@users.noreply.github.com>
2019-05-23 20:34:21 +0800

    Rename MapScene.h to GIT/proj.win32/MapScene1.h

---
+++ GIT/proj.win32/MapScene1.h
@@ -0,0 +1,24 @@
+#ifndef _MAP_SCENE_H_
+#define _MAP_SCENE_H_
+
+#include "cocos2d.h"
+#include "heroPrint.h"
+class MapScene : public cocos2d::Layer
+{
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+	heroPrint* hero;
+	//void update(float dt);
+	//void updatePosition(float dt);
+	virtual bool onTouchBegan(Touch* touch, Event* unused_event);
+	virtual void onKeyPressed(EventKeyboard::KeyCode keycode, Event* event);
+	void EnterHelloWorldScene(Ref* pSenderBack);
+
+	// implement the "static create()" method manually
+	CREATE_FUNC(MapScene);
+
+};
+
+#endif // !_MAP_SCENE_H_
